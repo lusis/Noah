78405b0e059022eb654c3cff673eeefd9996fd14
John E. Vincent <lusis.org+github.com@gmail.com>
2011-10-08 22:51:22 -0400

    tests for new json index page

--- spec/noahapp_spec.rb
+++ spec/noahapp_spec.rb
@@ -2,10 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Noah App Basics", :reset_redis => true do
 
-  it "should show the index page" do
+  it "should show the index page as html" do
+    header "Accept", "text/html"
     get '/'
     last_response.should be_ok
     last_response.body.include?("Noah Start Page").should  == true
+  end
+
+  it "should show the index page as json" do
+    header "Accept", "application/json"
+    get '/'
+    last_response.should return_json
+    response = JSON.parse(last_response.body)
+    response.has_key?("redis_version").should == true
+    response.has_key?("noah_version").should == true
+    response.has_key?("resources").should == true
   end
 
   it "should test the 404 message" do
